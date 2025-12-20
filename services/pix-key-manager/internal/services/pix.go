2fbe69d014db1eaf5ab0d5631e19362f473aaf4a
LXSCA <lucas.edr27@gmail.com>
2025-12-20 16:34:11 -0300

    feat: grpc service working with redis

--- services/pix-key-manager/internal/services/pix.go
+++ services/pix-key-manager/internal/services/pix.go
@@ -6,26 +6,37 @@ import (
 
 	pb "github.com/LXSCA7/pix-ledger/services/pix-key-manager/pb/proto"
 	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type PixGrpcService struct {
 	pb.UnimplementedPixServiceServer
-	keys map[string]string
+	client *redis.Client
 }
 
 func NewPixGrpcService() *PixGrpcService {
 	return &PixGrpcService{
-		keys: make(map[string]string),
+		client: redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379", // mudar pra env
+			Password: "",
+			DB:       0,
+		}),
 	}
 }
 
 func (s *PixGrpcService) FindKey(ctx context.Context, req *pb.PixRequest) (*pb.PixKeyResponse, error) {
 	fmt.Println("recebi req, chave", req.Key)
 
-	var userId = s.keys[req.Key]
-	if userId == "" {
+	userId, err := s.client.Get(ctx, req.Key).Result()
+	if err == redis.Nil {
 		return &pb.PixKeyResponse{Exists: false}, nil
+	} else if err != nil {
+		fmt.Printf("redis error %v\n", err)
+		return nil, status.Errorf(codes.Internal, "internal error")
 	}
+
 	return &pb.PixKeyResponse{
 		Exists: true,
 		Active: true,
@@ -40,12 +51,14 @@ func (s *PixGrpcService) CreateKey(ctx context.Context, req *pb.PixCreateKeyRequ
 		req.Key = uuid.New().String()
 	}
 
-	existant := s.keys[req.Key]
-	if existant != "" {
-		return &pb.PixCreateKeyResponse{}, fmt.Errorf("key already exists")
+	success, err := s.client.SetNX(ctx, req.Key, req.UserId, 0).Result()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "erro ao conectar no redis")
+	}
+	if !success {
+		return nil, status.Errorf(codes.AlreadyExists, "pix key '%s' already exists", req.Key)
 	}
 
-	s.keys[req.Key] = req.UserId
 	return &pb.PixCreateKeyResponse{
 		Key: req.Key,
 	}, nil
