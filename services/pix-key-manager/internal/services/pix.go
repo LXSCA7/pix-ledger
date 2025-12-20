d6f5f75fe25ba87d2e50db04787e3616b4662232
LXSCA <lucas.edr27@gmail.com>
2025-12-20 15:47:53 -0300

    feat: grpc server for create and get pix keys

--- services/pix-key-manager/internal/services/pix.go
+++ services/pix-key-manager/internal/services/pix.go
@@ -5,27 +5,48 @@ import (
 	"fmt"
 
 	pb "github.com/LXSCA7/pix-ledger/services/pix-key-manager/pb/proto"
+	"github.com/google/uuid"
 )
 
 type PixGrpcService struct {
 	pb.UnimplementedPixServiceServer
+	keys map[string]string
 }
 
 func NewPixGrpcService() *PixGrpcService {
-	return &PixGrpcService{}
+	return &PixGrpcService{
+		keys: make(map[string]string),
+	}
 }
 
 func (s *PixGrpcService) FindKey(ctx context.Context, req *pb.PixRequest) (*pb.PixKeyResponse, error) {
-	fmt.Printf("recebi req, chave", req.Key)
+	fmt.Println("recebi req, chave", req.Key)
 
-	if req.Key == "123" {
-		return &pb.PixKeyResponse{
-			Exists: true,
-			Active: true,
-			UserId: "user123",
-			Key:    "123",
-		}, nil
+	var userId = s.keys[req.Key]
+	if userId == "" {
+		return &pb.PixKeyResponse{Exists: false}, nil
+	}
+	return &pb.PixKeyResponse{
+		Exists: true,
+		Active: true,
+		UserId: userId,
+		Key:    req.Key,
+	}, nil
+}
+
+func (s *PixGrpcService) CreateKey(ctx context.Context, req *pb.PixCreateKeyRequest) (*pb.PixCreateKeyResponse, error) {
+	// vai virar um enum, somente testes por enquanto
+	if req.Key == "" && req.Kind == "random" {
+		req.Key = uuid.New().String()
 	}
 
-	return &pb.PixKeyResponse{Exists: false}, nil
+	existant := s.keys[req.Key]
+	if existant != "" {
+		return &pb.PixCreateKeyResponse{}, fmt.Errorf("key already exists")
+	}
+
+	s.keys[req.Key] = req.UserId
+	return &pb.PixCreateKeyResponse{
+		Key: req.Key,
+	}, nil
 }
